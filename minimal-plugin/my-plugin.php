0e633f6adb06ae6540dc6700776ac650a18b5e63
Eamon Burns (Laptop WSL) <me@eamonburns.com>
2024-11-21 13:20:20 -0800

    Initial commit

---
+++ minimal-plugin/my-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Minimal Plugin
+Plugin URI: https://github.com/Agent-E11/wp-plugin-dev
+Description: Minimal plugin to be replaced by your own
+Version: 0.0.0
+Author: Eamon Burns
+Author URI: https://github.com/Agent-E11
+License: GPL3
+*/
