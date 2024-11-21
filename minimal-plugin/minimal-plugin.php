d95f9f1c415d45c52bf8aa3e29457235e8a2b5e1
Eamon Burns (Laptop WSL) <me@eamonburns.com>
2024-11-21 13:28:23 -0800

    feat: rename plugin file and URI

--- minimal-plugin/minimal-plugin.php
+++ minimal-plugin/minimal-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Minimal Plugin
-Plugin URI: https://github.com/Agent-E11/wp-plugin-dev
+Plugin URI: https://github.com/Agent-E11/wp-minimal-plugin
 Description: Minimal plugin to be replaced by your own
 Version: 0.0.0
 Author: Eamon Burns
